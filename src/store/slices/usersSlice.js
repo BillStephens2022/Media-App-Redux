f80a42564dbbfd598173a30496a5dadd47712235
BillStephens2022 <two4onebill@yahoo.com>
2023-03-10 17:05:38 -0500

    added thunk function and extrareducers to usersSlice

--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -1,13 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { fetchUsers } from '../thunks/fetchUsers';
 
 const usersSlice = createSlice({
-    name: 'users',
-    initialState: {
-        data: [],
-        isLoading: false,
-        error: null
-    },
-    reducers: {}
+  name: "users",
+  initialState: {
+    data: [],
+    isLoading: false,
+    error: null,
+  },
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.pending, (state, action) => {
+        // update our state object to show user we are loading data
+        state.isLoading = true;
+    });
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+        // update state if data received successfully
+        state.isLoading = false;
+        state.data = action.payload;
+    });
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+        // update state if error with fetch request
+        state.isLoading = false;
+        state.error = action.error;
+    });
+  },
 });
 
 export const usersReducer = usersSlice.reducer;

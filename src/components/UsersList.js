cac3ce929cd3a218797059cb6638f8076aaa94c7
BillStephens2022 <two4onebill@yahoo.com>
2023-03-10 17:42:47 -0500

    added mapping function to render users on page

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,27 +1,37 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchUsers } from '../store';
-import Skeleton from './Skeleton';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "../store";
+import Skeleton from "./Skeleton";
 
 function UsersList() {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const { isLoading, data, error } = useSelector((state) => {
-        return state.users;  // { data: [], isLoading: false, error: null }
-    });
+  const { isLoading, data, error } = useSelector((state) => {
+    return state.users; // { data: [], isLoading: false, error: null }
+  });
 
-    useEffect(() => {
-        dispatch(fetchUsers());
-    }, [dispatch]);
-    
-    if (isLoading) {
-        return <Skeleton times={6} className="h-10 w-full" />;
-    }
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
 
-    if (error) {
-        return <div>Error fetching data...</div>
-    }
-    return <div>{data.length}</div>;
+  if (isLoading) {
+    return <Skeleton times={6} className="h-10 w-full" />;
+  }
+
+  if (error) {
+    return <div>Error fetching data...</div>;
+  }
+
+  const renderedUsers = data.map((user) => {
+    return (
+      <div key={user.id} className="mb-2 border rounded">
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          {user.name}
+        </div>
+      </div>
+    );
+  });
+  return <div>{renderedUsers}</div>;
 }
 
 export default UsersList;

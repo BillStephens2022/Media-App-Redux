e08c0e705d9f26f8b56fefb82417811b973a31a3
BillStephens2022 <two4onebill@yahoo.com>
2023-03-11 16:26:02 -0500

    added functionality to delete user and new component for UserListItem

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -2,10 +2,9 @@ import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
 import { useThunk } from "../hooks/use-thunk";
+import UsersListItem from "./UsersListItem";
 import Button from "./Button";
 import Skeleton from "./Skeleton";
-
-
 
 function UsersList() {
   const [doFetchUsers, isLoadingUsers, loadingUsersError] = useThunk(fetchUsers);
@@ -23,33 +22,27 @@ function UsersList() {
     doCreateUser();
   };
 
+  let content;
+
   if (isLoadingUsers) {
-    return <Skeleton times={6} className="h-10 w-full" />;
+    content = <Skeleton times={6} className="h-10 w-full" />;
+  } else if (loadingUsersError) {
+    content = <div>Error fetching data...</div>;
+  } else {
+    content = data.map((user) => {
+      return <UsersListItem key={user.id} user={user}/>
+      
+    });
   }
 
-  if (loadingUsersError) {
-    return <div>Error fetching data...</div>;
-  }
-
-  const renderedUsers = data.map((user) => {
-    return (
-      <div key={user.id} className="mb-2 border rounded">
-        <div className="flex p-2 justify-between items-center cursor-pointer">
-          {user.name}
-        </div>
-      </div>
-    );
-  });
   return (
     <div>
-      <div className="flex flex-row justify-between m-3">
+      <div className="flex flex-row justify-between items-center m-3">
         <h1 className="m-2 text-xl">Users</h1>
-        
-        <Button loading={isCreatingUser} onClick={handleUserAdd}> + Add User</Button>
-       
+        <Button loading={isCreatingUser} onClick={handleUserAdd}> + Add User</Button>    
         {creatingUserError && 'Error creating user...'}
       </div>
-      {renderedUsers}
+      {content}
     </div>
   );
 }
